508b2b233642b258b5fda771cfe90c236728ef93
Auridel <31318127+Auridel@users.noreply.github.com>
2020-12-05 12:13:03 +0300

    SQL transactions

--- src/db.js
+++ src/db.js
@@ -16,4 +16,52 @@ export default class DB {
             })
         })
     }
+    static getData() {
+        return new Promise(((resolve, reject) => {
+            db.transaction(tx => {
+                tx.executeSql(
+                    "SELECT * FROM memories",
+                    [],
+                    (_, res) => resolve(res.rows._array),
+                    (_, e) => reject(e)
+                )
+            })
+        }))
+    }
+    static addEntry({title, img, text}) {
+        return new Promise(((resolve, reject) => {
+            db.transaction(tx => {
+                tx.executeSql(
+                    "INSERT INTO memories(title, img, text, date, favorite) VALUES(?, ?, ? ,? ,?)",
+                    [title, img, text, Date.now(), false],
+                    (_, res) => resolve(res.insertId),
+                    (_, e) => reject(e)
+                )
+            })
+        }))
+    }
+    static updateEntry(id, favorite) {
+        return new Promise(((resolve, reject) => {
+            db.transaction(tx => {
+                tx.executeSql(
+                    "UPDATE memories SET favorite=? WHERE id=?",
+                    [favorite, id],
+                    resolve,
+                    (_, e) => reject(e)
+                )
+            })
+        }))
+    }
+    static deleteEntry(id) {
+        return new Promise(((resolve, reject) => {
+            db.transaction(tx => {
+                tx.executeSql(
+                    "DELETE FROM memories WHERE id=?",
+                    [id],
+                    resolve,
+                    (_, e) => reject(e)
+                )
+            })
+        }))
+    }
 }
